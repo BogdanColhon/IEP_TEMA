a03fb59b3804ed30218296ff949281027bbe3221
Bogdan <nbat99@yahoo.com>
2022-01-01 22:54:23 +0200

    New commit message

---
+++ build/src/CMakeFiles/T1.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/bogdan/Desktop/IEP/T1/src/lock.cpp" "/home/bogdan/Desktop/IEP/T1/build/src/CMakeFiles/T1.dir/lock.cpp.o"
+  "/home/bogdan/Desktop/IEP/T1/src/main2.cpp" "/home/bogdan/Desktop/IEP/T1/build/src/CMakeFiles/T1.dir/main2.cpp.o"
+  "/home/bogdan/Desktop/IEP/T1/src/order.cpp" "/home/bogdan/Desktop/IEP/T1/build/src/CMakeFiles/T1.dir/order.cpp.o"
+  "/home/bogdan/Desktop/IEP/T1/src/pizza.cpp" "/home/bogdan/Desktop/IEP/T1/build/src/CMakeFiles/T1.dir/pizza.cpp.o"
+  "/home/bogdan/Desktop/IEP/T1/src/pizzerie.cpp" "/home/bogdan/Desktop/IEP/T1/build/src/CMakeFiles/T1.dir/pizzerie.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/src/CMakeFiles/T1.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/T1.dir/lock.cpp.o"
+  "CMakeFiles/T1.dir/main2.cpp.o"
+  "CMakeFiles/T1.dir/order.cpp.o"
+  "CMakeFiles/T1.dir/pizza.cpp.o"
+  "CMakeFiles/T1.dir/pizzerie.cpp.o"
+  "T1"
+  "T1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/T1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
